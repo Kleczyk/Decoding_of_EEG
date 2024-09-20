91e7581a72b47041756ee22bbe3067826ffe74c3
Kleczyk <mailnasyff@gmail.com>
2024-09-20 21:41:33 +0200

    addition of sensor selection functionality

--- src/data/data_handler.py
+++ src/data/data_handler.py
@@ -6,11 +6,11 @@ import pywt
 
 
 class DataHandler:
-    def __init__(self, db_controller, num_exp=[3, 4], num_people=[1, 2]):
-        self.df = read_all_file_df(num_exp, num_people)
+    def __init__(self, db_controller, channels_names, idx_exp=[3, 4], idx_people=[1, 2]):
+        self.df = read_all_file_df(channels_names =channels_names, idx_exp=idx_exp, idx_people= idx_people)
         self.db_controller = db_controller
 
-    def make_cwt_transform(self, signal:np.array, wavelet_type, fmin, fmax, n_frex, length=640  ):
+    def make_cwt_transform(self, signal: np.array, wavelet_type, fmin, fmax, n_frex, length=640):
         time = np.linspace(0, length / 160, length)  # time of the signal , 160 is the sampling rate
         widths = np.geomspace(fmin, fmax, num=n_frex)  # range of scales
         sampling_period = np.diff(time).mean()  # 0.006251562890722681
@@ -28,6 +28,9 @@ class DataHandler:
         plt.colorbar()
         plt.show()
         return cwtmatr
+
+    def make_chanks(self):
+        pass
 
     def plot_cwt(self, signal, wavelet_type, fmin, fmax, n_frex, length):
         time = np.linspace(0, length / 160, length)  # time of the signal , 160 is the sampling rate
@@ -57,10 +60,11 @@ class DataHandler:
     def get_target(self):
         return self.df["target"].values
 
-
-db = DbController(dbname="my_db", user="user", password="1234", host="localhost", port="5433")
-cwt_data = DataHandler(db_controller=db)
-cwt_data.plot_cwt(cwt_data.df['Fc5'][0:640].to_numpy(), 'cgau4', 1, 200, 100, len(cwt_data.df['Fc5'][0:640].to_numpy()))
-print(cwt_data.df['Fc5'][0:640].to_numpy().shape)
-
-
+#
+# channels_names = ['Fc5.', 'Fc3.', 'Fc1.', 'Fcz.', 'Fc2.', 'Fc4.', 'Fc6.', 'C5..', 'C3..', 'C1..', 'Cz..', 'C2..', 'C4..', 'C6..', 'Cp5.', 'Cp3.', 'Cp1.', 'Cpz.', 'Cp2.', 'Cp4.', 'Cp6.', 'Fp1.', 'Fpz.', 'Fp2.', 'Af7.', 'Af3.', 'Afz.', 'Af4.', 'Af8.', 'F7..', 'F5..', 'F3..', 'F1..', 'Fz..', 'F2..', 'F4..', 'F6..', 'F8..', 'Ft7.', 'Ft8.', 'T7..', 'T8..', 'T9..', 'T10.', 'Tp7.', 'Tp8.', 'P7..', 'P5..', 'P3..', 'P1..', 'Pz..', 'P2..', 'P4..', 'P6..', 'P8..', 'Po7.', 'Po3.', 'Poz.', 'Po4.', 'Po8.', 'O1..', 'Oz..', 'O2..', 'Iz..']
+# channels_names = ['Fc5.', 'Af8.', 'P6..', 'P8..', 'Iz..']
+#
+#
+# db = DbController(dbname="my_db", user="user", password="1234", host="localhost", port="5433")
+# cwt_data = DataHandler(db_controller=db, channels_names=channels_names)
+# print(cwt_data.df)
--- src/data/read_data.py
+++ src/data/read_data.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 import mne
 
-
-def file_to_DataDrame(path):
+global_channels_names = ['Fc5.', 'Fc3.', 'Fc1.', 'Fcz.', 'Fc2.', 'Fc4.', 'Fc6.', 'C5..', 'C3..', 'C1..', 'Cz..', 'C2..', 'C4..', 'C6..', 'Cp5.', 'Cp3.', 'Cp1.', 'Cpz.', 'Cp2.', 'Cp4.', 'Cp6.', 'Fp1.', 'Fpz.', 'Fp2.', 'Af7.', 'Af3.', 'Afz.', 'Af4.', 'Af8.', 'F7..', 'F5..', 'F3..', 'F1..', 'Fz..', 'F2..', 'F4..', 'F6..', 'F8..', 'Ft7.', 'Ft8.', 'T7..', 'T8..', 'T9..', 'T10.', 'Tp7.', 'Tp8.', 'P7..', 'P5..', 'P3..', 'P1..', 'Pz..', 'P2..', 'P4..', 'P6..', 'P8..', 'Po7.', 'Po3.', 'Poz.', 'Po4.', 'Po8.', 'O1..', 'Oz..', 'O2..', 'Iz..']
+def file_to_DataDrame(path, channels_names=global_channels_names):
     """
     This function takes in a file path and returns a dataframe with the data and the target values
     format:
@@ -16,11 +16,12 @@ def file_to_DataDrame(path):
     reader = mne.io.read_raw_edf(path, preload=True)
     annotations = reader.annotations  # get the values of the annotations
     codes = annotations.description  # get the codes from the annotations
-
     df = pd.DataFrame(
         reader.get_data().T,
         columns=[channel.replace(".", "") for channel in reader.ch_names],
     )  # transpose the data to get the right shape
+    negation_channels_names = list(set(df.columns) - set([name.replace(".", "") for name in channels_names]))
+    df = df.drop(columns=negation_channels_names)  # remove the columns that are not in the channels_names list
     df = df[~(df == 0).all(axis=1)]  # remove rows with all zeros
     timeArray = np.array(
         [round(x, 10) for x in np.arange(0, len(df) / 160, 0.00625)]
@@ -41,7 +42,7 @@ def file_to_DataDrame(path):
     df["target"] = np.array(codeArray).T
     return df
 
-def read_all_file_df(num_exp=[3, 4], num_people=[1, 2], path="../../data/raw/"):
+def read_all_file_df(channels_names, idx_exp=[3, 4], idx_people=[1, 2], path="../../data/raw/"):
     """
     This function reads all the files in the path and returns a dataframe with the data and the target values
     format:
@@ -50,16 +51,17 @@ def read_all_file_df(num_exp=[3, 4], num_people=[1, 2], path="../../data/raw/"):
     1    -0.000054	-0.000048	-0.000034	...	0.000064	0.000114	0.000074	0
     ...
     Args:
-        num_exp (list): The list of experiments to read
-        num_people (list): The list of people to read
+        channels_names (list): The list of channels names
+        idx_exp (list): The list of experiments to read
+        idx_people (list): The list of people to read
         path (str): The path to the files
     Returns:
         pd.DataFrame: The dataframe containing the data and the target values
     """
     all_df = pd.DataFrame()
-    for subject in num_people:
-        for file in num_exp:
+    for subject in idx_people:
+        for file in idx_exp:
             fileName = f"{path}/S{subject:03d}/S{subject:03d}R{file:02d}.edf"
-            df = file_to_DataDrame(fileName)
+            df = file_to_DataDrame(fileName, channels_names)
             all_df = pd.concat([all_df, df], axis=0)
     return all_df
