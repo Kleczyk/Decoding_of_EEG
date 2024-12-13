9b293a32e5d511a8f8fec1c64074e8e680f92216
Kleczyk <mailnasyff@gmail.com>
2024-12-13 04:18:37 +0100

    god save the queen

--- src/training/tune_lighting_traning.py
+++ src/training/tune_lighting_traning.py
@@ -12,6 +12,7 @@ from lightning.pytorch.loggers import WandbLogger
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 import matplotlib.pyplot as plt
 import pandas as pd
+import wandb
 
 import data.read_data as rd
 from data.dataset import Dataset
@@ -19,50 +20,12 @@ from models.base_lstm_lighting import LSTM_base_lighting
 from data import DATA_PATH
 
 global_channels_names = [
-    "Fc5.",
-    "Fc3.",
-    "Fc1.",
-    "Af3.",
-    "Afz.",
-    "Af4.",
-    "Af8.",
-    "F7..",
-    "F5..",
-    "F3..",
-    "F1..",
-    "Fz..",
-    "F2..",
-    "F4..",
-    "F6..",
-    "F8..",
-    "Ft7.",
-    "Ft8.",
-    "T7..",
-    "T8..",
-    "T9..",
-    "T10.",
-    "Tp7.",
-    "Tp8.",
-    "P7..",
-    "P5..",
-    "P3..",
-    "P1..",
-    "Pz..",
-    "P2..",
-    "P4..",
-    "P6..",
-    "P8..",
-    "Po7.",
-    "Po3.",
-    "Poz.",
-    "Po4.",
-    "Po8.",
-    "O1..",
-    "Oz..",
-    "O2..",
-    "Iz..",
+    "Fc5.", "Fc3.", "Fc1.", "Af3.", "Afz.", "Af4.", "Af8.",
+    "F7..", "F5..", "F3..", "F1..", "Fz..", "F2..", "F4..", "F6..", "F8..",
+    "Ft7.", "Ft8.", "T7..", "T8..", "T9..", "T10.", "Tp7.", "Tp8.",
+    "P7..", "P5..", "P3..", "P1..", "Pz..", "P2..", "P4..", "P6..", "P8..",
+    "Po7.", "Po3.", "Poz.", "Po4.", "Po8.", "O1..", "Oz..", "O2..", "Iz..",
 ]
-
 
 def get_dataloaders(config: dict) -> (DataLoader, DataLoader):
     df_train = rd.read_all_file_df(channels_names=global_channels_names, idx_people=[1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -77,80 +40,71 @@ def get_dataloaders(config: dict) -> (DataLoader, DataLoader):
         df=df_val, sequence_length=config["seq_length"]
     )
     train_dataloader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True)
-    test_dataloader = DataLoader(val_dataset, batch_size=config["batch_size"])
+    val_dataloader = DataLoader(val_dataset, batch_size=config["batch_size"])
 
-    return train_dataloader, test_dataloader
+    return train_dataloader, val_dataloader
 
+def train_model(config):
+    wandb.init(project="EEG_Classification_finale", reinit=True)
+
+    train_loader, val_loader = get_dataloaders(config)
+
+    # Model
+    model = LSTM_base_lighting(
+        input_size=config["input_size"],
+        hidden_size=config["hidden_size"],
+        num_layers=config["num_layers"],
+        dropout=config["dropout"],
+        learning_rate=config["lr"],
+        num_classes=config["num_classes"]
+    )
+
+    # Logger dla Weight and Biases
+    wandb_logger = WandbLogger(project="EEG_Classification_finale")
+
+    # Trener PyTorch Lightning
+    trainer = pl.Trainer(
+        max_epochs=10,
+        logger=wandb_logger,
+        callbacks=[]
+    )
+
+    trainer.fit(model, train_loader, val_loader)
+
+    wandb.finish()
 
 def bayesian_optimization():
-    from ray import tune
-    from ray.tune.integration.pytorch_lightning import TuneReportCallback
-
     # Define the search space for hyperparameters
-
     search_space = {
         "lr": tune.loguniform(1e-5, 1e-1),
         "batch_size": tune.choice([8, 16, 32, 64, 128, 256, 512]),
         "hidden_size": tune.choice([32, 64, 128]),
-        "num_layers": tune.uniform(1, 100),
+        "num_layers": tune.randint(1, 4),
         "dropout": tune.uniform(0, 0.4),
-        "input_size": tune.qrandint(8, 1600),
+        "input_size": tune.randint(8, 1600),
         "num_classes": 3,
-        "seq_length": tune.qrandint(8, 1600),
+        "seq_length": tune.randint(8, 1600),
     }
 
-    def train_model(config):
-        train_set, val_set = get_dataloaders(config)
-        train_loader = DataLoader(train_set, batch_size=config["batch_size"])
-        val_loader = DataLoader(val_set, batch_size=config["batch_size"])
-
-        # Model
-        model = LSTM_base_lighting(
-            input_size=config["input_size"],
-            hidden_size=config["hidden_size"],
-            num_layers=config["num_layers"],
-            dropout=config["dropout"],
-            learning_rate=config["lr"],
-            num_classes=config["num_classes"]
-        )
-
-        # Logger dla Weight and Biases
-        wandb_logger = WandbLogger(project="EEG_Classification_final")
-
-        # Trener PyTorch Lightning
-        trainer = pl.Trainer(
-            storage_path=f"{DATA_PATH}/raw",
-            max_epochs=10,
-            logger=wandb_logger,
-            callbacks=[
-                TuneReportCallback({
-                    "val_loss": "val_loss",
-                    "val_acc": "val_acc",
-                    "val_f1": "val_f1"
-                })
-            ]
-        )
-
-        trainer.fit(model, train_loader, val_loader)
-
     scheduler = ASHAScheduler(
-
-        time_attr="training_iteration",  # Atrybut czasowy, np. liczba epok
-        max_t=100,  # Maksymalna liczba iteracji
-        grace_period=10,  # Minimalna liczba iteracji przed pruningiem
-        reduction_factor=3,  # Co 3-krotne zmniejszenie liczby prób
+        time_attr="training_iteration",
+        max_t=10,
+        grace_period=2,
+        reduction_factor=2,
     )
+
     analysis = tune.run(
         train_model,
         config=search_space,
         scheduler=scheduler,
         num_samples=100,
         metric="val_acc",
-        mode="max"
+        mode="max",
+        resources_per_trial={"cpu": 2, "gpu": 1},
     )
 
     print("Best hyperparameters found:", analysis.best_config)
 
-
 if __name__ == "__main__":
+    wandb.login()
     bayesian_optimization()
